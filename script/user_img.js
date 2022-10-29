e5af0c45076594e5430ff86878b818f5cb42e983
PaoloAffuso <p.affuso@gmail.com>
2022-10-29 14:33:31 +0200

    fine (mi ero dimenticato di tradurre i commenti)

--- script/user_img.js
+++ script/user_img.js
@@ -1,47 +1,35 @@
-//declearing html elements
-
+// dichiarazione di elementi html
 const imgDiv = document.querySelector('.profile-pic-div');
 const img = document.querySelector('#photo');
 const file = document.querySelector('#file');
 const uploadBtn = document.querySelector('#uploadBtn');
 
-//if user hover on img div 
+// sezione che gestisce se l'utente passa con il mouse su img div (hover)
 
 imgDiv.addEventListener('mouseenter', function(){
     uploadBtn.style.display = "block";
 });
 
-//if we hover out from img div
+// sezione che gestisce quando l'utente esce con il mouse da img div (hover)
 
 imgDiv.addEventListener('mouseleave', function(){
     uploadBtn.style.display = "none";
 });
 
-//lets work for image showing functionality when we choose an image to upload
-
-//when we choose a foto to upload
+//sezione relativa al caricamento della foto
 
 file.addEventListener('change', function(){
-    //this refers to file
+    // qui si fa riferimento al file
     const choosedFile = this.files[0];
 
     if (choosedFile) {
 
-        const reader = new FileReader(); //FileReader is a predefined function of JS
+        const reader = new FileReader();
 
         reader.addEventListener('load', function(){
             img.setAttribute('src', reader.result);
         });
 
         reader.readAsDataURL(choosedFile);
-
-        //Allright is done
-
-        //please like the video
-        //comment if have any issue related to vide & also rate my work in comment section
-
-        //And aslo please subscribe for more tutorial like this
-
-        //thanks for watching
     }
 });
