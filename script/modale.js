c478cddb5b28557eec4c45473ff6bc0d2a9451af
PaoloAffuso <p.affuso@gmail.com>
2022-11-11 21:29:36 +0100

    Implementazione di: - popup student per inviare richieste di iscrizione - popup teacher gestione iscrizioni - completamento di sezione info student e teacher (manca solo il popup per modificare i campi,     aspettiamo Natale che sta per farlo)

--- script/modale.js
+++ script/modale.js
@@ -9,3 +9,23 @@ const toggleModal = () => {
         classList.add("open");
     }
 }
+
+const toggleModal_1 = () => {
+    const {classList} = document.body;
+    if(classList.contains("open")){
+        classList.remove("open");
+        classList.add("closed");
+    }
+    else{
+        classList.remove("closed");
+        classList.add("open");
+    }
+}
+
+function cbChange(obj) {
+    var cbs = document.getElementsByClassName("cb");
+    for (var i = 0; i < cbs.length; i++) {
+        cbs[i].checked = false;
+    }
+    obj.checked = true;
+}
