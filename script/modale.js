fb6c87ba52bfae929ff612619e0e2075643e49d1
PaoloAffuso <p.affuso@gmail.com>
2022-11-13 20:01:41 +0100

    Avanzamento implementazione di: - popup aggiunta corso teacher (aggiunti numero di capitoli e immagine da caricare)
    Aggiunta opzione "seleziona tutti" nel popup di gestione richieste di iscrizione

--- script/modale.js
+++ script/modale.js
@@ -10,18 +10,6 @@ const toggleModal = () => {
     }
 }
 
-const toggleModal_1 = () => {
-    const {classList} = document.body;
-    if(classList.contains("open")){
-        classList.remove("open");
-        classList.add("closed");
-    }
-    else{
-        classList.remove("closed");
-        classList.add("open");
-    }
-}
-
 function cbChange(obj) {
     var cbs = document.getElementsByClassName("cb");
     for (var i = 0; i < cbs.length; i++) {
@@ -29,3 +17,35 @@ function cbChange(obj) {
     }
     obj.checked = true;
 }
+
+
+// Sezione dedicata alla selezione di tutti gli elementi della lista pending requests
+const selectAll = document.querySelector('.form-group.select-all input');
+const allCheckbox = document.querySelectorAll('.form-group:not(.select-all) input');
+let listBoolean = [];
+
+allCheckbox.forEach(item=> {
+    item.addEventListener('change', function () {
+        allCheckbox.forEach(i=> {
+            listBoolean.push(i.checked);
+        })
+        if(listBoolean.includes(false)) {
+            selectAll.checked = false;
+        } else {
+            selectAll.checked = true;
+        }
+        listBoolean = []
+    })
+})
+
+selectAll.addEventListener('change', function () {
+    if(this.checked) {
+        allCheckbox.forEach(i=> {
+            i.checked = true;
+        })
+    } else {
+        allCheckbox.forEach(i=> {
+            i.checked = false;
+        })
+    }
+})
