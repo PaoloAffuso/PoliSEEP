9d15718ee3b2d9604030a07c87fb54b1df672321
giulialimo <giulia.limongelli99@gmail.com>
2022-12-12 14:51:49 +0100

    chat sx

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // si definiscono le variabili per la connessione al database in locale
 // quando tale file è caricato su Altervista, possiede i parametri relativi al database in remoto 
-define('DB_SERVER', 'localhost:3308'); // indirizzo server
+define('DB_SERVER', 'localhost:3306'); // indirizzo server
 define('DB_USERNAME', 'root'); // username che si utilizza per accedere al database
 define('DB_PASSWORD', ''); // password che si utilizza per accedere al database
 define('DB_NAME', 'poliseep'); // nome del database
