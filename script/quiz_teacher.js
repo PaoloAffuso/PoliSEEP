b93168e7ad5cd848e7de3c99fe0db53c69ddcd1c
GaiaBellifemine <gaga2108@hotmail.it>
2023-03-06 20:22:30 +0100

    implementazione quiz-doc pt1
    Inizio gestione domanda radio: acquisizione (+stampa) dei dati dal form.

--- script/quiz_teacher.js
+++ script/quiz_teacher.js
@@ -2,6 +2,8 @@ import {db, storage} from '../firebaseConfig.js';
 import {ref, child, get, query, equalTo, orderByChild, set, update, push} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 import {uploadBytes, ref as sRef, getDownloadURL, deleteObject, listAll, getMetadata} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-storage.js';
 
+window.submitQuiz=submitQuiz;
+
 if (localStorage.getItem("email") === null) {
     window.location.href = "/poliseep";
 }
@@ -14,10 +16,7 @@ window.onload = function(){
     let type = getLoggedType(username);
     if(type==="STU") window.location.href = "/poliseep/student/student.html"; //Pagina student ancora da fare
 
-    var container_div = document.getElementById('questions');
-    console.log(container_div);
-    var count = container_div.getElementsByTagName('section').length;
-    console.log(count);
+
 };
 
 async function getLoggedType(username) {
@@ -27,6 +26,57 @@ async function getLoggedType(username) {
         type=element.val().tipo;
     });
     return type;
+}
+
+function submitQuiz(){
+    var quizName = document.getElementById("namequiz").value;
+    var quizDesc = document.getElementById("descriptionquiz").value;
+    //var quizChap = document.getElementById("namequiz").value;
+
+    var container_div = document.getElementById('questions');
+    console.log(container_div);
+    var count = container_div.getElementsByTagName('section').length;
+    console.log(count);
+    console.log(quizName);
+    console.log(quizDesc);
+
+    for(let i=1; i<=count; i++){
+        var question = document.querySelector("#d"+i+" .first2 #wrapper1 #question").value;
+        var questionType = document.querySelector("#d"+i+" .first2 #wrapper2 #questiontype").value;
+        console.log(question);
+        console.log(questionType);
+        switch(questionType)
+        {
+            case "radioquestion": 
+                var nanswerssaq = document.querySelector("#d"+i+" #saq #nanswerssaq").value;
+                var answers = []; // l'affermazione
+                var checked = []; // indica se l'affermazione è corretta
+                var explain = []; // indica perché l'affermazione è sbagliata
+
+                for(let j=1; j<=nanswerssaq; j++){
+                    console.log(document.querySelector("#d"+i+" #saq .vanswers #answer"+j).value);
+                    answers[j-1] = document.querySelector("#d"+i+" #saq .vanswers #answer"+j).value;
+                    checked[j-1] = document.querySelector("#d"+i+" #saq .vanswers #check"+j).checked;
+                    explain[j-1] = document.querySelector("#d"+i+" #saq .vanswers #explain"+j).value;
+                }
+                console.log(answers);
+                console.log(checked);
+                console.log(explain);
+            break;
+
+            case "checkboxquestion": 
+                console.log("2");
+            break;
+
+            case "rispaperta": 
+                console.log("3");
+            break;
+
+            default:
+                alert("Select the question type!");
+            break;
+        }
+    }
 }
 
 
